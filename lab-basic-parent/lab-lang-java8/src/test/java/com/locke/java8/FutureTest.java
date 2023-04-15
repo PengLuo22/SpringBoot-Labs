5cc5e2f7659aaddae9171c5ffa1f1d5f86058521
PengLuo22 <luopeng1995@126.com>
2023-04-15 23:40:59 +0800

    feat: 增加lab-lang-java8 项目,针对性的联系java8的新特性

---
+++ lab-basic-parent/lab-lang-java8/src/test/java/com/locke/java8/FutureTest.java
@@ -0,0 +1,25 @@
+package com.locke.java8;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * @author PengLuo22
+ * @date 2023/4/15 22:32
+ * @since 0.0.1
+ */
+public class FutureTest {
+
+
+    @Test
+    void completableFuture() {
+        // CompletableFuture
+
+        CompletableFuture completableFuture = new CompletableFuture();
+
+
+        // CompletionStage
+
+    }
+}
---
+++ lab-basic-parent/lab-lang-java8/src/test/java/com/locke/java8/StreamTest.java
@@ -0,0 +1,20 @@
+package com.locke.java8;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author PengLuo22
+ * @date 2023/4/15 22:30
+ * @since 0.0.1
+ */
+@SpringBootTest
+public class StreamTest {
+
+
+
+
+
+
+
+
+}
